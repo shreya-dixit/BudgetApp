af375021e8bdff7ea5a33243eaccd54010d8d033
Shreya <shreyadevotee@gmail.com>
2023-07-07 17:14:58 +0530

    Remove submodule entry

--- ContactsApp/App.js
+++
@@ -1,9 +0,0 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import AppNavigator from './src/AppNavigator';
-
-const App = () => {
-  return <AppNavigator />;
-};
-
-export default App;
